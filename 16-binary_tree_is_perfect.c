4bb29f3fd4f1ddb26f0588e78a4ec08fd6d1dd42
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-11-02 16:57:15 +0000

    fixed perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,39 +1,4 @@
 #include "binary_trees.h"
-/**
- * hleft - measures height to left of tree
- *
- * @tree: pointer to the root node of the binary tree
- * Return: height of tree to left of tree
- */
-int hleft(const binary_tree_t *tree)
-{
-	int hleft2;
-	int hright2;
-
-	if (tree == NULL)
-		return (-1);
-	hleft2 = tree->left ? 1 + hleft(tree->left) : 0;
-	hright2 = tree->right ? 1 + hleft(tree->right) : 0;
-	return (hleft2 > hright2 ? hleft2 : hright2);
-}
-
-/**
- * hright - measures height to right of tree
- *
- * @tree: pointer to the root node of the binary tree
- * Return: height of tree to right of tree
- */
-int hright(const binary_tree_t *tree)
-{
-	int hleft2;
-	int hright2;
-
-	if (tree == NULL)
-		return (-1);
-	hleft2 = tree->left ? 1 + hright(tree->left) : 0;
-	hright2 = tree->right ? 1 + hright(tree->right) : 0;
-	return (hleft2 > hright2 ? hleft2 : hright2);
-}
 
 /**
  * binary_tree_is_perfect - measures if a binary tree is perfect
@@ -44,13 +9,13 @@ int hright(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int height_left = hleft(tree->left);
-	int height_right = hright(tree->right);
-
 	if (tree == NULL)
 		return (0);
 
-	if (height_left != height_right)
-		return (0);
-	return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+
+	return (0);
 }
